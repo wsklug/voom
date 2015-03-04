49669db0edc9400cc95d7a013a3f363c0d0bcc65
Amit Singh <amit112amit@g.ucla.edu>
2015-03-03 16:57:22 -0800

    Added files and folder for ConeProblem under Applications

--- src/Applications/Capsid/IndentLJ/saveThisRun.sh
+++ src/Applications/Capsid/IndentLJ/saveThisRun.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 
 # This script saves the files created as a result of running a
-# simulation into ./GoodRuns
+# simulation into $SAVETO
 # -Amit
 
 # The following two variables store the directory name and the list of
 # files to be removed
-DIRECTORY=.
+
 SAVEFILES=('abnormal*.vtk' 'contact*.vtk' 'lbfgsb*.vtk' \
 '*-body*step*.vtk' '*.fz' '*.initial*.vtk' '*.relaxed*.vtk' \
-'indent.log')
+'*.output.*' '*.joblog.*')
 
 # We are passing all elements of $SAVEFILES array and piping the
 # output of ls to null device to make its execution silent
 #echo 'Following files will be saved:'
 #ls ${SAVEFILES[@]} 2>/dev/null
 
-echo 'What folder to save these files in ./GoodRuns/?'
-read FOLDERNAME
+# Current directory is the default source directory
+defSrcDir=$PWD
+srcDir=""${1:-$defSrcDir}
+echo "Source directory: $srcDir"
 
-NEWFOLDER="./GoodRuns/$FOLDERNAME"
+defTarDir=~/Research/Results/VirusIndentation/${srcDir##*/}
+target=${2:-$defTarDir}
+echo "Target directory: ${target}"
 
-if [ ! -d "$folderName" ]
-then
-    mkdir -p "$NEWFOLDER"
-    mv ${SAVEFILES[@]} "$NEWFOLDER" 2>/dev/null
-    cd "$NEWFOLDER"
-    tar -zcvf "$FOLDERNAME.tar.gz" *
-    mv "$FOLDERNAME.tar.gz" ../
-    echo 'Simulation archived successfully!'
-else
-    echo 'Something went wrong... try debugging or do it manually :-P'
-fi
+mkdir -p $target
+
+for i in ${SAVEFILES[@]}
+do
+    mv ${srcDir}/${i} "$target" 2>/dev/null         
+done
+
+cd "$target"
+tar -zcvf "${target##*/}.tar.gz" *
+mv "${target##*/}.tar.gz" ../
+echo 'Simulation archived successfully!'
---
+++ src/Applications/ConeProblem/coneproblem.cc
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+#include <time.h>
+#include <vector>
+#include <fstream>
+#include <unistd.h>
+#include <tvmet/Vector.h>
+#include "Model.h"
+
+#include <vtkDataSet.h>
+#include <vtkPointData.h>
+#include <vtkPolyData.h>
+#include <vtkDataSetReader.h>
+#include <vtkPolyDataWriter.h>
+#include <vtkPolyDataNormals.h>
+#include <vtkSmartPointer.h>
+#include <vtkUnstructuredGrid.h>
+#include <vtkGeometryFilter.h>
+#include <vtkSetGet.h>
+#include <vtkExtractEdges.h>
+#include <vtkCellArray.h>
+#include <vtkIdList.h>
+#include <vtkUnsignedIntArray.h>
+#include <vtkCell.h>
+
+using namespace tvmet;
+using namespace std;
+using namespace voom;
+
+int main(int argc, char* argv[])
+{
+  return 0;
+}
