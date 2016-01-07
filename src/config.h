2343ad3352c430c6411c66b19c011a07543f165b
LouisFoucard <foucard@colorado.edu>
2016-01-06 20:01:07 -0800

    add untracked files

---
+++ src/config.h
@@ -0,0 +1,29 @@
+/* src/config.h.  Generated from config.h.in by configure.  */
+/* src/config.h.in.  Generated from configure.in by autoheader.  */
+
+/* Define if you have a Blitz installation */
+#define HAVE_BLITZ 1
+
+/* Define if you have a tvmet installation */
+#define HAVE_TVMET 1
+
+/* Name of package */
+#define PACKAGE "voom"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "klug@ucla.edu"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "voom"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "voom 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "voom"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Version number of package */
+#define VERSION "1.0"
